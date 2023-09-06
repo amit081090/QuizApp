1ea97b1b193aadfe631999ebe38a2b7ea64481ba
Amit Bahuguna <bahugunaamit8@gmail.com>
2023-09-06 07:44:48 +0530

    my Quiz app

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,37 @@
+export const Questions = [
+{
+
+    prompt:"what is 1+3",
+    optionA:"24",
+    optionB:"21",
+    optionC:"4",
+    optionD:"2",
+    answer:"C"
+
+},
+
+
+
+{
+
+    prompt:"which is a streaming plateform",
+    optionA:"Twitter",
+    optionB:"Twitch",
+    optionC:"Google",
+    optionD:"Robinhood",
+    answer:"B"
+
+},
+
+{
+
+    prompt:"Best Tech Youtube Channel",
+    optionA:"PedroTech",
+    optionB:"DEV ED",
+    optionC:"FireShip",
+    optionD:"TechWithHim",
+    answer:"A"
+
+},
+
+]
