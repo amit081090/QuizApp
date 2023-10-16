cf3abeefde567ec9689dcad20f16ada9700196b6
Amit Bahuguna <bahugunaamit8@gmail.com>
2023-10-16 06:20:15 +0530

    update focus color

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -1,4 +1,4 @@
-import React,{useState,useContext} from 'react';
+import React,{useState,useContext,useRef} from 'react';
 import {Questions} from '../Helpers/QuestionBank';
 import { QuizContext } from '../Helpers/Contexts';
 
@@ -7,6 +7,7 @@ const Quiz = () => {
   const{score,setScore,setGameState}=useContext(QuizContext)
   const[currQuestion,setCurrQuestion]=useState(0);
   const[optionChosen,setOptionChosen]=useState("");
+  const selectedOptionRef= useRef(null);
 
   const nextQuestion=()=>{
 
@@ -15,6 +16,7 @@ const Quiz = () => {
     }
     
     setCurrQuestion(currQuestion + 1);
+    selectedOptionRef.current.style.backgroundColor = '';
 
   };
 
@@ -24,23 +26,66 @@ const Quiz = () => {
 }
 setGameState("EndScreen")
   };
-  return (
-    <div className='Quiz'>
-      <h1>{Questions[currQuestion].prompt}</h1>
-      <div className='options'>
 
-        <button onClick={()=>setOptionChosen("A")}>{Questions[currQuestion].optionA}</button>
-        <button onClick={()=>setOptionChosen("B")}>{Questions[currQuestion].optionB}</button>
-        <button onClick={()=>setOptionChosen("C")}>{Questions[currQuestion].optionC}</button>
-        <button  onClick={()=>setOptionChosen("D")}>{Questions[currQuestion].optionD}</button>
-      </div>
-      {currQuestion === Questions.length - 1 ? 
-      (<button onClick={finishQuiz}>Finish QUIZ</button>)
-      :<button onClick={nextQuestion}>Next Question</button>}
+  const handleOptionClick = (option) => {
+    setOptionChosen(option);
+
+    // Reset the style for the previously selected option
+    if (selectedOptionRef.current) {
+      selectedOptionRef.current.style.backgroundColor = '';
+    }
+
+    // Set the style for the newly selected option
+    selectedOptionRef.current = document.getElementById(option);
+    if (selectedOptionRef.current) {
+      selectedOptionRef.current.style.backgroundColor = 'royalblue'; // Change this to the desired color
+    }
+  };
+  return (
+//     <div className='Quiz'>
+//       <h1>{Questions[currQuestion].prompt}</h1>
+//       <div className='options'>
+
+//         <button id="A"onClick={()=>setOptionChosen("A")}>{Questions[currQuestion].optionA}</button>
+//         <button id="B"onClick={()=>setOptionChosen("B")}>{Questions[currQuestion].optionB}</button>
+//         <button id="C"onClick={()=>setOptionChosen("C")}>{Questions[currQuestion].optionC}</button>
+//         <button id="D" onClick={()=>setOptionChosen("D")}>{Questions[currQuestion].optionD}</button>
+//       </div>
+//       {currQuestion === Questions.length - 1 ? 
+//       (<button onClick={finishQuiz}>Finish QUIZ</button>)
+//       :<button onClick={nextQuestion}>Next Question</button>}
     
 
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
+
+
+
+
+<div className='Quiz'>
+<h1>{Questions[currQuestion].prompt}</h1>
+<div className='options'>
+  <button id="A" onClick={() => handleOptionClick("A")}>
+    {Questions[currQuestion].optionA}
+  </button>
+  <button id="B" onClick={() => handleOptionClick("B")}>
+    {Questions[currQuestion].optionB}
+  </button>
+  <button id="C" onClick={() => handleOptionClick("C")}>
+    {Questions[currQuestion].optionC}
+  </button>
+  <button id="D" onClick={() => handleOptionClick("D")}>
+    {Questions[currQuestion].optionD}
+  </button>
+</div>
+{currQuestion === Questions.length - 1 ? (
+  <button onClick={finishQuiz}>Finish QUIZ</button>
+) : (
+  <button onClick={nextQuestion}>Next Question</button>
+)}
+</div>
+);
+};
 
 export default Quiz;
